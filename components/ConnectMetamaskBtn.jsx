90277c605b5e04e4bd1e6662d4f8239144733507
Karin <karin.stenwall@familjen.se>
2022-06-28 09:25:39 +0200

    feat: add next and typescript

--- components/ConnectMetamaskBtn.jsx
+++ components/ConnectMetamaskBtn.jsx
@@ -3,15 +3,11 @@ import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
 
 const ConnectMetamaskBtn = () => {
   const connectWithMetamask = useMetamask();
-  const address = useAddress();
+
   return (
-    <div>
-      {address ? (
-        <h4>Connected as {address}</h4>
-      ) : (
-        <button onClick={connectWithMetamask}>Connect Metamask Wallet</button>
-      )}
-    </div>
+    <>
+      <button onClick={connectWithMetamask}>Connect Metamask Wallet</button>
+    </>
   );
 };
 

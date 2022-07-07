de11cf4294b52e87aa5cce45007b37f2027b9d25
stenwall <karin.stenwall@gmail.com>
2022-07-07 16:45:18 +0200

    feat: working claim phases info

--- components/PhaseInfo.tsx
+++ components/PhaseInfo.tsx
@@ -1,7 +1,59 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
+import { ClaimCondition } from '@thirdweb-dev/sdk';
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
 import React from 'react';
 
-const PhaseInfo = () => {
+interface PhaseInfoProps {
+  activeClaim: ClaimCondition;
+  allClaims: ClaimCondition[];
+}
+
+const PhaseInfo: React.FC<PhaseInfoProps> = ({ activeClaim, allClaims }) => {
+  const bnPrice = parseUnits(
+    activeClaim?.currencyMetadata.displayValue || '0',
+    activeClaim?.currencyMetadata.decimals
+  );
+  const priceToMint = bnPrice.mul(1);
+
+  const getDate = dateTime => {
+    const date = new Date(dateTime);
+    return date.toLocaleString('en-GB', {
+      timeZone: 'PST',
+      day: 'numeric',
+      month: 'long'
+    });
+  };
+
+  const getTime = (thisClaimTime, nextClaimTime?) => {
+    const thisTime = new Date(thisClaimTime);
+    const time = thisTime.toLocaleString('en-US', {
+      timeZone: 'PST',
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+    if (nextClaimTime) {
+      const nextTime = new Date(nextClaimTime);
+      const hours = Math.abs(nextTime.getTime() - thisTime.getTime()) / 36e5;
+      return `${time} PST / ${Math.round(hours)}H`;
+    }
+    return `${time} PST`;
+  };
+
+  const getPrice = currencyMetadata => {
+    return currencyMetadata.displayValue
+      ? `\u2261 ${formatUnits(priceToMint, currencyMetadata.decimals)} ${
+          activeClaim?.currencyMetadata.symbol
+        }`
+      : '';
+  };
+
+  const getQuantity = maxQuantity => {
+    console.log(maxQuantity);
+    return maxQuantity === 'unlimited'
+      ? maxQuantity
+      : `Max ${maxQuantity} / wallet`;
+  };
+
   return (
     <>
       <Flex
@@ -10,64 +62,78 @@ const PhaseInfo = () => {
         gap={2}
         w="100%"
       >
-        <Box
-          bg="#DFDFCF"
-          w="100%"
-          pl="20px"
-          pr="10px"
-          py="15px"
-          color="#000000"
-          borderRadius="15px"
-          height="100"
-        >
-          <Text fontSize="10px" align="left" casing="uppercase">
-            8 July
-            <br />
-            11:00 AM PST / 24H
-            <br />
-            SMIRCS GENESIS HOLDERS
-          </Text>
-        </Box>
-        <Box
-          bg="#CFCFB1"
-          w="100%"
-          pl="20px"
-          pr="10px"
-          py="15px"
-          color="#000000"
-          borderRadius="15px"
-          height="100"
-        >
-          <Text fontSize="10px" align="left" casing="uppercase">
-            9 July
-            <br />
-            11:00 AM PST / 24H
-            <br />
-            PREMINT ACCESSLIST/WL
-            <br />
-            &#8801; 0.15 ETH
-          </Text>
-        </Box>
-        <Box
-          bg="#C3C2AA"
-          w="100%"
-          pl="20px"
-          pr="10px"
-          py="15px"
-          color="#000000"
-          borderRadius="15px"
-          height="100"
-        >
-          <Text fontSize="10px" align="left" casing="uppercase">
-            10 July
-            <br />
-            11:00 AM PST / 24H
-            <br />
-            Public
-            <br />
-            &#8801; 0.25 ETH
-          </Text>
-        </Box>
+        {allClaims[1] && (
+          <Box
+            bg="#DFDFCF"
+            w="100%"
+            pl="20px"
+            pr="10px"
+            py="15px"
+            color="#000000"
+            borderRadius="15px"
+            height="110"
+          >
+            <Text fontSize="10px" align="left" casing="uppercase">
+              <>
+                {getDate(allClaims[1]?.startTime)}
+                <br />
+                {getTime(allClaims[1]?.startTime, allClaims[2]?.startTime)}
+                <br />
+                SMIRCS GENESIS HOLDERS
+                <br />
+                {getQuantity(allClaims[1]?.maxQuantity)}
+              </>
+            </Text>
+          </Box>
+        )}
+        {allClaims[2] && (
+          <Box
+            bg="#CFCFB1"
+            w="100%"
+            pl="20px"
+            pr="10px"
+            py="15px"
+            color="#000000"
+            borderRadius="15px"
+            height="110"
+          >
+            <Text fontSize="10px" align="left" casing="uppercase">
+              {getDate(allClaims[2]?.startTime)}
+              <br />
+              {getTime(allClaims[2]?.startTime, allClaims[3]?.startTime)}
+              <br />
+              ACCESSLIST/WL
+              <br />
+              {getPrice(allClaims[2]?.currencyMetadata)}
+              <br />
+              {getQuantity(allClaims[2]?.maxQuantity)}
+            </Text>
+          </Box>
+        )}
+        {allClaims[3] && (
+          <Box
+            bg="#C3C2AA"
+            w="100%"
+            pl="20px"
+            pr="10px"
+            py="15px"
+            color="#000000"
+            borderRadius="15px"
+            height="110"
+          >
+            <Text fontSize="10px" align="left" casing="uppercase">
+              {getDate(allClaims[3]?.startTime)}
+              <br />
+              {getTime(allClaims[3]?.startTime)}
+              <br />
+              Public
+              <br />
+              {getPrice(allClaims[3]?.currencyMetadata)}
+              <br />
+              {getQuantity(allClaims[3]?.maxQuantity)}
+            </Text>
+          </Box>
+        )}
       </Flex>
     </>
   );
